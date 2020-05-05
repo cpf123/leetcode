0126a103ed632d569f4cd375f3541a281f09af81
Fay <1044913875@qq.com>
2020-05-05 19:15:59 +0800

    二叉树

--- src/main/java/树/leetcode_98.java
+++ src/main/java/树/leetcode_98.java
@@ -30,24 +30,34 @@ public class leetcode_98 {
      * 解释: 输入为: [5,1,4,null,null,3,6]。
      *      根节点的值为 5 ，但是其右子节点值为 4 。
      *
+     *      *     4
+     *      *    / \
+     *      *   1   6
+     *      *      / \
+     *      *     5   7
+     *
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
     //递归
-    public boolean isBST(TreeNode node, Integer low, Integer up) {
+    public boolean isvalidBST(TreeNode node, Integer low, Integer up) {
         if (node == null) {
-            return true;
+            return true; // 空节点是二叉搜索树
         }
         int val = node.val;
-        if (low != null && low > val) {
+        if (low != null && low > val) {  // 判断节点的值 是否在其上下界
             return false;
         }
         if (up != null && up < val) {
             return false;
         }
-        if(isBST(node.right,val,up)){ return false}
+        if(!isvalidBST(node.right,val,up)){ return false;}  //  将当前节点的值替换为下届 继续检查右边的子节点 是二叉树 则 !isBST(node.right,val,up)=false
+        if(!isvalidBST(node.left,low,val)){ return false;} // left <val  left> low (上级节点)
+        return true;
     }
+    // 中序遍历
+
 
 }
